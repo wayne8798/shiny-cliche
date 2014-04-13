44c4fc18fae5fc260335b55ca22e34734f2d0778
SudoAttack <wayne8798@gmail.com>
2014-04-12 22:43:31 -0500

    add py server

---
+++ py_server.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy
+import zeropc
+
+class FaceDetector(object):
+    def detect(img_data):
+        fh = open('temp.png', 'wb')
+        fh.write((img_data[21:]).decode('base64'))
+        fh.close()
+        img = cv2.imread('temp.png')
+        cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+        rects = cascade.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
+        return rects
+
+s = zerorpc.Server(FaceDetector())
+s.bind('tcp://0.0.0.0:4242')
+s.run()
